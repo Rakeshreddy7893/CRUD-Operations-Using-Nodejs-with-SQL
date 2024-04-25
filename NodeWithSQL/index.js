cb3c9ab7e9596ca610397a383067858e2b10c811
Rakeshreddy7893 <rakeshreddykalagiri@gmail.com>
2024-04-25 18:29:22 +0530

    Done with CRUD Operations using Nodejs With SQL

--- NodeWithSQL/index.js
+++ NodeWithSQL/index.js
@@ -1,6 +1,14 @@
 const{faker} = require('@faker-js/faker');
 const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -9,24 +17,90 @@ const connection = mysql.createConnection({
   password:'root'
 });
 
-try{
-  connection.query("SHOW TABLES",(err,result)=>{
+let getRandomUser=()=>{
+  return [
+   faker.string.uuid(),
+   faker.internet.userName(),
+   faker.internet.email(),
+   faker.internet.password(),
+];
+}
+
+
+app.get('/',(req,res)=>{
+  let q = `SELECT COUNT(*) FROM user`;
+
+  try{
+  connection.query(q,(err,result)=>{
     if(err) throw err;
-    console.log(result);
+    let count = result[0]["COUNT(*)"];
+    res.render("home.ejs",{count});
   })
   
 }catch(err){
   console.log(err);
-}
+  res.send("some error in DB");
+};
+})
 
-connection.end();
+app.get('/user',(req,res)=>{
+   let q = `SELECT * FROM user`;
+   try{
+    connection.query(q,(err,users)=>{
+      if(err) throw err;
+      res.render("showusers.ejs",{users});
+    })
+    
+  }catch(err){
+    console.log(err);
+    res.send("some error in DB");
+  };
+})
 
- let getRandomUser=()=>{
-    return {
-      id: faker.string.uuid(),
-      username: faker.internet.userName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-}
+app.get("/user/:id/edit",(req,res)=>{
+  let {id} = req.params;
+  let q = `SELECT * FROM user WHERE id='${id}'`
+  try{
+    connection.query(q,(err,result)=>{
+      if(err) throw err;
+      let user = result[0];
+      res.render("edit.ejs",{user});
+    })
+    
+  }catch(err){
+    console.log(err);
+    res.send("some error in DB");
+  };
+})
+
+
+app.patch("/user/:id",(req,res)=>{
+  let {id} = req.params;
+  let {password:formPass,username:newUsername} = req.body;
+  let q = `SELECT * FROM user WHERE id='${id}'`
+
+  try{
+    connection.query(q,(err,result)=>{
+      if(err) throw err;
+      let user = result[0];
+      if(formPass  != user.password){
+        res.send("WRONG PASSWORD");
+      }else{
+        let q2 = `UPDATE user SET username='${newUsername}'  WHERE id='${id}'`;
+        connection.query(q2,(err,result)=>{
+          if(err) throw err
+          res.redirect("/user");
+        })
+      }
+    })
+    
+  }catch(err){
+    console.log(err);
+    res.send("some error in DB");
+  };
+})
+
+ app.listen("8080",()=>{
+  console.log("listening at :8080");
+ })
 
